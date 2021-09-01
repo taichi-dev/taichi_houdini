2f02b0a0ccd0ab1d2fcb1a2264dab4391541e09e
Yadi Cao <elliott.yd.cao@outlook.com>
2021-09-01 13:27:22 +0800

    pipeline working; but bug still exists

--- Libs/python/mpm_solver_shell.py
+++ Libs/python/mpm_solver_shell.py
@@ -12,38 +12,56 @@ class MPMSolverShell:
         self.solver = MPMSolver(res_array)
 
     @ti.func
-    def detailed_seed_one_channel_data(self, i, channel: ti.template(),
-                                       data: ti.template()):
+    def write_channel_data(self, i, channel: ti.template(),
+                           data: ti.template()):
         channel[i] = data
 
     @ti.kernel
-    def detailed_seed_one_scalar_channel(self, new_pnum: ti.i32,
-                                         channel: ti.template(),
-                                         data: ti.ext_arr()):
+    def write_scalar_channel(self, new_pnum: ti.i32, channel: ti.template(),
+                             data: ti.ext_arr()):
         for i in range(self.solver.n_particles[None],
                        self.solver.n_particles[None] + new_pnum):
-            self.detailed_seed_one_channel_data(i, channel, data[i])
+            self.write_channel_data(i, channel, data[i])
 
     @ti.kernel
-    def detailed_seed_one_vector_channel(self, new_pnum: ti.i32,
-                                         channel: ti.template(),
-                                         data: ti.ext_arr()):
+    def write_vector_channel(self, new_pnum: ti.i32, channel: ti.template(),
+                             data: ti.ext_arr()):
         for i in range(self.solver.n_particles[None],
                        self.solver.n_particles[None] + new_pnum):
             vec = ti.Vector([data[i, d] for d in range(self.dim)])
-            self.detailed_seed_one_channel_data(i, channel, vec)
+            self.write_channel_data(i, channel, vec)
 
     @ti.kernel
-    def detailed_seed_one_matrix_channel(self, new_pnum: ti.i32,
-                                         channel: ti.template(),
-                                         data: ti.ext_arr()):
+    def write_matrix_channel(self, new_pnum: ti.i32, channel: ti.template(),
+                             data: ti.ext_arr()):
         for i in range(self.solver.n_particles[None],
                        self.solver.n_particles[None] + new_pnum):
             mat = ti.Matrix.zero(ti.f32, self.dim, self.dim)
             for r in ti.static(range(self.dim)):
                 for c in ti.static(range(self.dim)):
                     mat[r, c] = data[i, r, c]
-            self.detailed_seed_one_channel_data(i, channel, mat)
+            self.write_channel_data(i, channel, mat)
+
+    @ti.kernel
+    def read_scalar_channel(self, channel: ti.template(),
+                            ext_channel: ti.ext_arr()):
+        for i in range(self.solver.n_particles[None]):
+            ext_channel[i] = channel[i]
+
+    @ti.kernel
+    def read_vector_channel(self, channel: ti.template(),
+                            ext_channel: ti.ext_arr()):
+        for i in range(self.solver.n_particles[None]):
+            for d in ti.static(range(self.dim)):
+                ext_channel[i, d] = channel[i][d]
+
+    @ti.kernel
+    def read_matrix_channel(self, channel: ti.template(),
+                            ext_channel: ti.ext_arr()):
+        for i in range(self.solver.n_particles[None]):
+            for d in ti.static(range(self.dim)):
+                for d2 in ti.static(range(self.dim)):
+                    ext_channel[i, d, d2] = channel[i][d, d2]
 
     def detailed_append_from_ext_array(self, P, v, F, Jp, C, material, density,
                                        E, nu, f_angle, H):
@@ -63,24 +81,57 @@ class MPMSolverShell:
         assert np.shape(H)[0] == new_pnum, "wrong num for H"
 
         print('append channels')
-        self.detailed_seed_one_vector_channel(new_pnum, self.solver.x, P)
-        self.detailed_seed_one_vector_channel(new_pnum, self.solver.v, v)
-        self.detailed_seed_one_matrix_channel(new_pnum, self.solver.F, F)
-        self.detailed_seed_one_scalar_channel(new_pnum, self.solver.Jp, Jp)
-        self.detailed_seed_one_matrix_channel(new_pnum, self.solver.C, C)
-        self.detailed_seed_one_scalar_channel(new_pnum, self.solver.material,
-                                              material)
+        self.write_vector_channel(new_pnum, self.solver.x, P)
+        self.write_vector_channel(new_pnum, self.solver.v, v)
+        self.write_matrix_channel(new_pnum, self.solver.F, F)
+        self.write_scalar_channel(new_pnum, self.solver.Jp, Jp)
+        self.write_matrix_channel(new_pnum, self.solver.C, C)
+        self.write_scalar_channel(new_pnum, self.solver.material, material)
         # TODO until ti end support per particle parameters
         # NOTE remember to convert parameters
-        # self.detailed_seed_one_scalar_channel(new_pnum, self.solver.density,
+        # self.write_scalar_channel(new_pnum, self.solver.density,
         #                                       density)
-        # self.detailed_seed_one_scalar_channel(new_pnum, self.solver.E, E)
-        # self.detailed_seed_one_scalar_channel(new_pnum, self.solver.nu, nu)
-        # self.detailed_seed_one_scalar_channel(new_pnum, self.solver.f_angle,
+        # self.write_scalar_channel(new_pnum, self.solver.E, E)
+        # self.write_scalar_channel(new_pnum, self.solver.nu, nu)
+        # self.write_scalar_channel(new_pnum, self.solver.f_angle,
         #                                       f_angle)
-        # self.detailed_seed_one_scalar_channel(new_pnum, self.solver.H, H)
+        # self.write_scalar_channel(new_pnum, self.solver.H, H)
 
         self.solver.n_particles[None] += new_pnum
+
+    def detailed_export_to_ext_array(self):
+
+        p_num = self.solver.n_particles[None]
+        print("resize ext arr")
+        P = np.zeros((p_num, self.dim))
+        print("P shape inside ", np.shape(P))
+        v = np.zeros((p_num, self.dim))
+        F = np.zeros((p_num, self.dim, self.dim))
+        C = np.zeros((p_num, self.dim, self.dim))
+        Jp = np.zeros(p_num)
+        material = np.zeros(p_num)
+        density = np.zeros(p_num)
+        E = np.zeros(p_num)
+        nu = np.zeros(p_num)
+        f_angle = np.zeros(p_num)
+        H = np.zeros(p_num)
+
+        print('export channels')
+        self.read_vector_channel(self.solver.x, P)
+        self.read_vector_channel(self.solver.v, v)
+        self.read_matrix_channel(self.solver.F, F)
+        self.read_scalar_channel(self.solver.Jp, Jp)
+        self.read_matrix_channel(self.solver.C, C)
+        self.read_scalar_channel(self.solver.material, material)
+        # TODO until ti end support per particle parameters
+        # NOTE remember to convert parameters
+        # self.read_scalar_channel(self.solver.density, density)
+        # self.read_scalar_channel(self.solver.E, E)
+        # self.read_scalar_channel(self.solver.nu, nu)
+        # self.read_scalar_channel(self.solver.f_angle, f_angle)
+        # self.read_scalar_channel(self.solver.H, H)
+
+        return P, v, F, Jp, C, material, density, E, nu, f_angle, H
 
 
 if __name__ == "__main__":
@@ -101,3 +152,5 @@ if __name__ == "__main__":
     H = np.zeros(160)
     ms2.detailed_append_from_ext_array(P, v, F, Jp, C, material, density, E,
                                        nu, f_angle, H)
+    ms2.detailed_export_to_ext_array(P, v, F, Jp, C, material, density, E, nu,
+                                     f_angle, H)
